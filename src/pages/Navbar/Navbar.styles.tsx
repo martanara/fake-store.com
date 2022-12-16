5ed966e5fb70185141b8c6dc0927a2a505e742a2
Marta Nara <marta.nara.gt@gmail.com>
2022-12-16 18:37:09 +0100

    Fix RWD in navbar

--- src/pages/Navbar/Navbar.styles.tsx
+++ src/pages/Navbar/Navbar.styles.tsx
@@ -29,10 +29,6 @@ export const StyledNavLink = styled(NavLink)`
     &:hover {
         opacity: 0.5;
     }
-
-    @media (max-width: 768px) {
-        padding: 0;
-    }
 `;
 
 export const StyledNavLinkActive = styled(StyledNavLink)`
@@ -47,18 +43,19 @@ export const StyledMenu = styled.div<StyledMenuProps>`
     justify-content: space-between;
     align-items: center;
 
-    @media (max-width: 768px) {
+    @media (max-width: 900px) {
         display: ${({ isOpen }) => (isOpen ? "flex" : "none")};
         flex-direction: column;
         justify-content: space-evenly;
         overflow: hidden;
         width: 100%;
-        height: 170px;
+        height: 300px;
         transition: max-height 0.3s ease-in;
     }
 `;
 
 export const StyledLogo = styled(StyledNavLink)`
+   padding: 10px;
    font-weight: 700;
    font-size: 20px;
    color: ${(props) => props.theme.colors.lightGreen}
@@ -67,7 +64,7 @@ export const StyledLogo = styled(StyledNavLink)`
 export const StyledIconButton = styled(IconButton)`
     display: none;
     
-    @media (max-width: 768px) {
+    @media (max-width: 900px) {
         display: flex;
     }
 `;
